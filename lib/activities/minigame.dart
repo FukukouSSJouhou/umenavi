8a6e83ed27ba15a5b6ac73650435ee08c4642419
kokkiemouse <kokkiemouse@fascode.net>
2021-05-18 22:53:36 +0900

    added

--- lib/activities/minigame.dart
+++ lib/activities/minigame.dart
@@ -5,8 +5,21 @@ class MiniGame1 extends StatefulWidget{
   _MiniGame1State createState()=>new _MiniGame1State();
 }
 class _MiniGame1State extends State<MiniGame1>{
-  String No1State="On";
-  String No2State="On";
+  String No1State="Off";
+  String No2State="Off";
+  String No3State="Off";
+  String No4State="Off";
+  String No5State="Off";
+  String No6State="Off";
+  String No7State="Off";
+  String No8State="Off";
+  String No9State="Off";
+  String No10State="Off";
+  String No11State="Off";
+  String No12State="Off";
+  String No13State="Off";
+  String No14State="Off";
+  String No15State="Off";
   @override
   Widget build(BuildContext context){
     return new Scaffold(
@@ -16,47 +29,273 @@ class _MiniGame1State extends State<MiniGame1>{
       body:GridView.count(
         crossAxisCount: 5,
     children: <Widget>[
-      miniGameTapkun(
-        onTap:() {
-          showDialog(context: context, builder: (context){
-            if(No2State == "On"){
-              No2State="Off";
-            }else{
-              No2State="On";
-            }
-            return SimpleDialog(
-              title: Text("Oh"),
-              children: [
-                SimpleDialogOption(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text("Tapped 1"),
-                ),
-              ],
-            );
-          });
-        },
-        text:No1State
-      ),
-      miniGameTapkun(
-        onTap:() {
-          if(No1State == "On"){
-            No1State="Off";
-          }else{
-            No1State="On";
+      new miniGameTapkun(No1State,() {
+        setState(() {
+
+          if (No1State == "On") {
+            No1State = "Off";
+          } else {
+            No1State = "On";
           }
-          showDialog(context: context, builder: (context){
-            return SimpleDialog(
-              title: Text("Oh"),
-              children: [
-                SimpleDialogOption(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text("Tapped 2"),
-                ),
-              ],
-            );
-          });
+
+          if (No2State == "On") {
+            No2State = "Off";
+          } else {
+            No2State = "On";
+          }
+
+          if (No6State == "On") {
+            No6State = "Off";
+          } else {
+            No6State = "On";
+          }
+        });
+      }),
+      new miniGameTapkun(
+        No2State,() {
+
+
+        setState(() {
+          if(No1State == "On"){
+      No1State = "Off";
+          }else{
+      No1State = "On";
+          }
+          if(No3State == "On"){
+            No3State = "Off";
+          }else{
+            No3State = "On";
+          }
+
+          if (No2State == "On") {
+            No2State = "Off";
+          } else {
+            No2State = "On";
+          }
+
+          if (No7State == "On") {
+            No7State = "Off";
+          } else {
+            No7State = "On";
+          }
+        });
         },
-        text:No2State
+      ),
+      new miniGameTapkun(
+        No3State,() {
+
+
+        setState(() {
+          if(No4State == "On"){
+            No4State = "Off";
+          }else{
+            No4State = "On";
+          }
+          if(No3State == "On"){
+            No3State = "Off";
+          }else{
+            No3State = "On";
+          }
+
+          if (No2State == "On") {
+            No2State = "Off";
+          } else {
+            No2State = "On";
+          }
+
+          if (No8State == "On") {
+            No8State = "Off";
+          } else {
+            No8State = "On";
+          }
+        });
+      },
+      ),
+      new miniGameTapkun(
+        No4State,() {
+
+
+        setState(() {
+          if(No4State == "On"){
+            No4State = "Off";
+          }else{
+            No4State = "On";
+          }
+          if(No3State == "On"){
+            No3State = "Off";
+          }else{
+            No3State = "On";
+          }
+
+          if (No5State == "On") {
+            No5State = "Off";
+          } else {
+            No5State = "On";
+          }
+
+          if (No9State == "On") {
+            No9State = "Off";
+          } else {
+            No9State = "On";
+          }
+        });
+      },
+      ),      new miniGameTapkun(
+        No5State,() {
+
+
+        setState(() {
+          if(No4State == "On"){
+            No4State = "Off";
+          }else{
+            No4State = "On";
+          }
+
+          if (No5State == "On") {
+            No5State = "Off";
+          } else {
+            No5State = "On";
+          }
+
+          if (No10State == "On") {
+            No10State = "Off";
+          } else {
+            No10State = "On";
+          }
+        });
+      },
+      ),new miniGameTapkun(
+        No6State,() {
+
+
+        setState(() {
+          if(No1State == "On"){
+            No1State = "Off";
+          }else{
+            No1State = "On";
+          }
+
+          if (No7State == "On") {
+            No7State = "Off";
+          } else {
+            No7State = "On";
+          }
+
+          if (No6State == "On") {
+            No6State = "Off";
+          } else {
+            No6State = "On";
+          }
+          if(No11State == "On"){
+            No11State = "Off";
+          }else{
+            No11State = "On";
+          }
+        });
+      },
+      ),
+      new miniGameTapkun(
+        No7State,() {
+
+
+        setState(() {
+          if(No2State == "On"){
+            No2State = "Off";
+          }else{
+            No2State = "On";
+          }
+
+          if (No7State == "On") {
+            No7State = "Off";
+          } else {
+            No7State = "On";
+          }
+
+          if (No6State == "On") {
+            No6State = "Off";
+          } else {
+            No6State = "On";
+          }
+          if(No12State == "On"){
+            No12State = "Off";
+          }else{
+            No12State = "On";
+          }
+          if(No8State == "On"){
+            No8State = "Off";
+          }else{
+            No8State = "On";
+          }
+        });
+      },
+      ),      new miniGameTapkun(
+        No8State,() {
+
+
+        setState(() {
+          if(No3State == "On"){
+            No3State = "Off";
+          }else{
+            No3State = "On";
+          }
+
+          if (No7State == "On") {
+            No7State = "Off";
+          } else {
+            No7State = "On";
+          }
+
+          if (No9State == "On") {
+            No9State = "Off";
+          } else {
+            No9State = "On";
+          }
+          if(No13State == "On"){
+            No13State = "Off";
+          }else{
+            No13State = "On";
+          }
+          if(No8State == "On"){
+            No8State = "Off";
+          }else{
+            No8State = "On";
+          }
+        });
+      },
+      ),new miniGameTapkun(
+        No9State,() {
+
+
+        setState(() {
+          if(No4State == "On"){
+            No4State = "Off";
+          }else{
+            No4State = "On";
+          }
+
+          if (No10State == "On") {
+            No10State = "Off";
+          } else {
+            No10State = "On";
+          }
+
+          if (No9State == "On") {
+            No9State = "Off";
+          } else {
+            No9State = "On";
+          }
+          if(No14State == "On"){
+            No14State = "Off";
+          }else{
+            No14State = "On";
+          }
+          if(No8State == "On"){
+            No8State = "Off";
+          }else{
+            No8State = "On";
+          }
+        });
+      },
       )
     ]
       )
--- lib/widgets/minigameTapkun.dart
+++ lib/widgets/minigameTapkun.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
-class miniGameTapkun extends StatelessWidget{
-  const miniGameTapkun({
-  @required String text,
+class miniGameTapkun extends StatefulWidget {
+  /*const miniGameTapkun({
+    @required String text,
     @required Function onTap
-})
-  :this._text=text,
-  this._onTap=onTap;
+  })
+      :this._text=text,
+        this._onTap=onTap;
   final String _text;
-  final Function _onTap;
+  final Function _onTap;*/
+  final String text;
+  final Function onTap;
+  miniGameTapkun(this.text,this.onTap);
+  @override
+  _miniGameTapkunState createState()=>_miniGameTapkunState();
+}
+class _miniGameTapkunState extends State<miniGameTapkun>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return InkWell(
       onTap: (){
-        _onTap();
+        widget.onTap();
       },
-      child:Center(child:Text(_text,
+      child:Center(child:Text(widget.text,
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 40,
