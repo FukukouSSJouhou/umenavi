f95a94e0f4942b8d02954025a49100bb920b6642
kokkiemouse <kokkiemouse@fascode.net>
2021-06-01 20:59:46 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'activities/miniGame.dart';
 import "widgets/CustomAppBar.dart";
 import "activities/SecretPage.dart";
 String applicationName= "UMENAVI";
-String applicationVersion= "20210601beta2";
+String applicationVersion= "20210601beta2.1";
 String applicationLegalese="2021 Fukushima High School SS club Jouhouhan";
 void main() {
   runApp(MyApp());
@@ -23,20 +23,10 @@ class MyApp extends StatelessWidget {
     const locale = Locale("ja", "JP");
     return MaterialApp(
       title: 'UMENAVI',
-      /*darkTheme: ThemeData(
-        primaryColor: Color.fromRGBO(0,0,0,1),
-      ),
+      /*darkTheme: ThemeData.dark(),
       */
-      theme: ThemeData.dark(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      theme:ThemeData(
+        primaryColor: Color.fromRGBO(0,0,0,1),
       ),
 
       locale: locale,
