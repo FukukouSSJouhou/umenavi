987d2edf39fe0bd2f41aee6950c77d18427834ba
kokkiemouse <kokkiemouse@fascode.net>
2021-06-04 07:01:12 +0900

    grid

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
       ),
-      body: ListView(
+      body: GridView(
         children: [
           InkWell(
               onTap: (){
