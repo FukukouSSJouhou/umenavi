b41e88abe13687154521cd7925f09911d5194394
kokkiemouse <kokkiemouse@fascode.net>
2021-06-05 21:23:23 +0900

    fixed

--- android/app/src/main/java/com/fukukou/umezitu/umenavi/MainActivity.java
+++ android/app/src/main/java/com/fukukou/umezitu/umenavi/MainActivity.java
@@ -1,6 +1,6 @@
 package com.fukukou.umezitu.umenavi;
 
 import io.flutter.embedding.android.FlutterActivity;
-
+import io.flutter.plugins.urllauncher.UrlLauncherPlugin;
 public class MainActivity extends FlutterActivity {
 }
---
+++ android/app/src/main/kotlin/com/fukukou/umezitu/umezitu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fukukou.umezitu.umezitu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
