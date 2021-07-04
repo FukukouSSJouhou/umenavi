543162d7383fe43978526fb2b717129ddb6520b4
kokkiemouse <kokkiemouse@fascode.net>
2021-07-04 22:46:03 +0900

    added enji

--- lib/themes/ThemeData.dart
+++ lib/themes/ThemeData.dart
@@ -3,3 +3,6 @@ final ThemeData ippanDarkTheme=ThemeData.dark();
 final ThemeData ippanLightTheme=ThemeData(
   primaryColor: Color.fromRGBO(41, 30, 161,1),
 );
+final ThemeData EnjiTheme=ThemeData(
+  primaryColor: Color.fromRGBO(139, 0, 0,1)
+);
