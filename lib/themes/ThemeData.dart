e7b95a846330cc93b12e360d27a567067bdad223
kokkiemouse <kokkiemouse@fascode.net>
2021-07-04 22:43:30 +0900

    added theme

--- lib/themes/ThemeData.dart
+++ lib/themes/ThemeData.dart
@@ -1,1 +1,5 @@
 import 'package:flutter/material.dart';
+final ThemeData ippanDarkTheme=ThemeData.dark();
+final ThemeData ippanLightTheme=ThemeData(
+  primaryColor: Color.fromRGBO(41, 30, 161,1),
+);
