2a56691851238b062cba911daa5d80153f3e7394
kokkiemouse <kokkiemouse@fascode.net>
2021-07-05 06:51:12 +0900

    fixed

--- lib/activities/settings/Settings.dart
+++ lib/activities/settings/Settings.dart
@@ -23,7 +23,7 @@ class _SettingsPageState extends State<SettingsPage>{
             ),),
           SafeArea(
             child: ThemeListView(),
-          );
+          ),
         ],
       )
     );
