47139dbe8d4d6576e14736017f993db1a3ebf636
kokkiemouse <kokkiemouse@fascode.net>
2021-07-05 17:03:43 +0900

    fixed

--- run_iphone.sh
+++ run_iphone.sh
@@ -2,4 +2,4 @@
 gitidkun=`git rev-parse --short HEAD`
 dtkun=`date "+%Y%M%D %H%m%s"`
 Buildid="${gitidkun} ${dtkun}"
-flutter run --release --dart-define=APPVER=$Buildid
+flutter run --release --dart-define=APPVER="${Buildid}"
