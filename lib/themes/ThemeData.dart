72a0db8967682c42fa2e521915a3c5c9b30aae1c
kokkiemouse <kokkiemouse@fascode.net>
2021-07-08 08:22:32 +0900

    added

--- lib/themes/ThemeData.dart
+++ lib/themes/ThemeData.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 final ThemeData ippanDarkTheme=ThemeData.dark();
 final ThemeData ippanLightTheme=ThemeData(
   primaryColor: Color.fromRGBO(41, 30, 161,1),
 );
 final ThemeData EnjiTheme=ThemeData(
-  primaryColor: Color.fromRGBO(139, 0, 0,1)
+  primaryColor: Color.fromRGBO(139, 0, 0,1),
+  appBarTheme: AppBarTheme(
+    backwardsCompatibility: false,
+    systemOverlayStyle: SystemUiOverlayStyle.light
+  )
 );
