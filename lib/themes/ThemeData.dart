d3c8dcfa3671ee293bf62d46905724adc62ae01f
kokkiemouse <kokkiemouse@fascode.net>
2021-07-10 22:46:48 +0900

    fixed color

--- lib/themes/ThemeData.dart
+++ lib/themes/ThemeData.dart
@@ -3,6 +3,11 @@ import 'package:flutter/services.dart';
 final ThemeData ippanDarkTheme=ThemeData.dark();
 final ThemeData ippanLightTheme=ThemeData(
   primaryColor: Color.fromRGBO(41, 30, 161,1),
+  appBarTheme: AppBarTheme(
+    backwardsCompatibility: false,
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    backgroundColor: Color.fromRGBO(41, 30, 161, 1)
+  )
 );
 final ThemeData EnjiTheme=ThemeData(
   primaryColor: Color.fromRGBO(139, 0, 0,1),
