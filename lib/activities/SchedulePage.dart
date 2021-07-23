28c29b63184228148ae67ed5fb8a3ae2e9d56475
kokkiemouse <kokkiemouse@fascode.net>
2021-07-23 10:40:26 +0900

    omit
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- lib/activities/SchedulePage.dart
+++ lib/activities/SchedulePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_timetable_view/flutter_timetable_view.dart';
 class SchedulePage extends StatefulWidget{
   @override
   _SchedulePageState createState()=>new _SchedulePageState();
@@ -11,67 +10,12 @@ class _SchedulePageState extends State<SchedulePage>{
       appBar:AppBar(
         title: Text("Schedule"),
       ),
-      body: TimetableView(
-        laneEventsList: [
-          LaneEvents(
-            lane: Lane(name:"行事(明確にわかるものしか載せていません)"),
-            events: [
-              /*
-              TableEvent(
-                title:"会場オープン",
-                start:TableEventTime(hour: 7,minute: 30),
-                end:TableEventTime(hour: 8,minute: 0),
-              ),
-              TableEvent(
-                title:"SHR",
-                start:TableEventTime(hour: 8,minute: 20),
-                end:TableEventTime(hour: 8,minute: 45)
-              ),
-              TableEvent(
-                title: "部活動発表",
-                start:TableEventTime(hour: 8,minute: 45),
-                end: TableEventTime(hour: 9,minute: 0)
-              ),
-              TableEvent(
-                title:"開祭式",
-                start: TableEventTime(hour:9,minute: 0),
-                end: TableEventTime(hour: 9,minute: 30),
-              ),
-               */
-              TableEvent(
-                title:"クラス企画",
-                start: TableEventTime(hour: 9,minute: 30),
-                end: TableEventTime(hour:10,minute: 48),
-              ),
-              TableEvent(
-                title:"休憩",
-                start: TableEventTime(hour:10,minute: 48),
-                end:TableEventTime(hour:11,minute: 00),
-              ),
-              TableEvent(
-                title:"クラス企画",
-                start: TableEventTime(hour: 11,minute: 00),
-                end:TableEventTime(hour: 12,minute: 18)
-              ),
-              TableEvent(
-                title:"昼休み",
-                start:TableEventTime(hour: 12,minute: 18),
-                end: TableEventTime(hour:13,minute: 00),
-              ),
-              TableEvent(
-                title:"クラス企画",
-                start: TableEventTime(hour:13,minute: 00),
-                end:TableEventTime(hour: 14,minute: 20),
-              ),
-            ]
+      body: ListView(
+        children: [
+          InkWell(
+            child:Image.asset("images/others/TimeTable_2.png"),
           )
         ],
-        timetableStyle: TimetableStyle(
-          startHour: 9,
-          endHour: 15,
-            timeItemHeight:180,
-            visibleDecorationBorder:true,
-        ),
       )
     );
   }
