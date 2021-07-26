aa5737104a87f47536e8c7a183f5e2dcc67388ea
kokkiemouse <kokkiemouse@fascode.net>
2021-07-27 07:20:46 +0900

    added code

---
+++ lib/activities/vote/VoteStart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class VoteStartPage extends StatefulWidget{
+  @override
+  _VoteStartPageState createState ()=> _VoteStartPageState();
+}
+class _VoteStartPageState extends State<VoteStartPage>{
+  Widget build(BuildContext context){
+    return(
+        ListView(
+
+      )
+    );
+  }
+}
