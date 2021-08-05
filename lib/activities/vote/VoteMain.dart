3ed3aa2ba462436676c9ede0ac9b0c95451bfbff
kokkiemouse <kokkiemouse@fascode.net>
2021-08-05 09:42:05 +0900

    added

--- lib/activities/vote/VoteMain.dart
+++ lib/activities/vote/VoteMain.dart
@@ -1,3 +1,5 @@
+import 'package:UMENAVI/activities/vote/Voting.dart';
+import 'package:UMENAVI/bunkasaidbkun/BunkasaiDBSet.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,30 +17,6 @@ class VoteMainPage extends StatefulWidget{
 
   VoteMainPage({Key key,this.google_signin,this.auth}):super(key: key);
   _VoteMainPageState createState()=>_VoteMainPageState();
-}
-enum BunkasaiClasskun{
-  C11,
-  C12,
-  C13,
-  C14,
-  C15,
-  C16,
-  C17,
-  C21,
-  C22,
-  C23,
-  C24,
-  C25,
-  C26,
-  C27,
-  C31,
-  C32,
-  C33,
-  C34,
-  C35,
-  C36,
-  C37,
-  NONEKUN
 }
 
 class _VoteMainPageState extends State<VoteMainPage>{
@@ -317,6 +295,15 @@ class _VoteMainPageState extends State<VoteMainPage>{
       appBar: AppBar(
         title:Text("Vote"),
         actions: [
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: (){
+              HapticFeedback.heavyImpact();
+              Navigator.push(context,MaterialPageRoute(builder: (_) =>
+                  VotingPage()
+              ));
+            },
+          ),
           PopupMenuButton<int>(
             itemBuilder: (context2)=>[
               PopupMenuItem(
@@ -335,149 +322,6 @@ class _VoteMainPageState extends State<VoteMainPage>{
         ],
       )
     );
-  }
-  String BunkasaiClasskun_to_str(BunkasaiClasskun b1){
-    String resultkun="";
-    switch(b1){
-      case BunkasaiClasskun.C11:
-        return "1-1";
-        break;
-      case BunkasaiClasskun.C12:
-        return "1-2";
-        break;
-      case BunkasaiClasskun.C13:
-        return "1-3";
-        break;
-      case BunkasaiClasskun.C14:
-        return "1-4";
-        break;
-      case BunkasaiClasskun.C15:
-        return "1-5";
-        break;
-      case BunkasaiClasskun.C16:
-        return "1-6";
-        break;
-      case BunkasaiClasskun.C17:
-        return "1-7";
-        break;
-      case BunkasaiClasskun.C21:
-        return "2-1";
-        break;
-      case BunkasaiClasskun.C22:
-        return "2-2";
-        break;
-      case BunkasaiClasskun.C23:
-        return "2-3";
-        break;
-      case BunkasaiClasskun.C24:
-        return "2-4";
-        break;
-      case BunkasaiClasskun.C25:
-        return "2-5";
-        break;
-      case BunkasaiClasskun.C26:
-        return "2-6";
-        break;
-      case BunkasaiClasskun.C27:
-        return "2-7";
-        break;
-      case BunkasaiClasskun.C31:
-        return "3-1";
-        break;
-      case BunkasaiClasskun.C32:
-        return "3-2";
-        break;
-      case BunkasaiClasskun.C33:
-        return "3-3";
-        break;
-      case BunkasaiClasskun.C34:
-        return "3-4";
-        break;
-      case BunkasaiClasskun.C35:
-        return "3-5";
-        break;
-      case BunkasaiClasskun.C36:
-        return "3-6";
-        break;
-      case BunkasaiClasskun.C37:
-        return "3-7";
-        break;
-      case BunkasaiClasskun.NONEKUN:
-        return "none";
-        break;
-    }
-    return resultkun;
-  }
-  BunkasaiClasskun str_to_bunkasaiClass(String txtun){
-    switch(txtun){
-      case "1-1":
-        return BunkasaiClasskun.C11;
-        break;
-      case "1-2":
-        return BunkasaiClasskun.C12;
-        break;
-      case "1-3":
-        return BunkasaiClasskun.C13;
-        break;
-      case "1-4":
-        return BunkasaiClasskun.C14;
-        break;
-      case "1-5":
-        return BunkasaiClasskun.C15;
-        break;
-      case "1-6":
-        return BunkasaiClasskun.C16;
-        break;
-      case "1-7":
-        return BunkasaiClasskun.C17;
-        break;
-      case "2-1":
-        return BunkasaiClasskun.C21;
-        break;
-      case "2-2":
-        return BunkasaiClasskun.C22;
-        break;
-      case "2-3":
-        return BunkasaiClasskun.C23;
-        break;
-      case "2-4":
-        return BunkasaiClasskun.C24;
-        break;
-      case "2-5":
-        return BunkasaiClasskun.C25;
-        break;
-      case "2-6":
-        return BunkasaiClasskun.C26;
-        break;
-      case "2-7":
-        return BunkasaiClasskun.C27;
-        break;
-      case "3-1":
-        return BunkasaiClasskun.C31;
-        break;
-      case "3-2":
-        return BunkasaiClasskun.C32;
-        break;
-      case "3-3":
-        return BunkasaiClasskun.C33;
-        break;
-      case "3-4":
-        return BunkasaiClasskun.C34;
-        break;
-      case "3-5":
-        return BunkasaiClasskun.C35;
-        break;
-      case "3-6":
-        return BunkasaiClasskun.C36;
-        break;
-      case "3-7":
-        return BunkasaiClasskun.C37;
-        break;
-      case "none":
-        return BunkasaiClasskun.NONEKUN;
-        break;
-    }
-    return BunkasaiClasskun.C15;
   }
   Future<void> loginkun(BuildContext context) async{
     try {
--- lib/activities/vote/Voting.dart
+++ lib/activities/vote/Voting.dart
@@ -1,1 +1,63 @@
+import 'package:UMENAVI/bunkasaidbkun/BunkasaiDBSet.dart';
 import "package:flutter/material.dart";
+class VotingPage extends StatefulWidget{
+  final String user_id;
+  final String user_email;
+  final BunkasaiClasskun GoodCls1;
+  final BunkasaiClasskun GoodCls2;
+  final BunkasaiClasskun GoodCls3;
+  final BunkasaiClasskun MVPCls;
+  final BunkasaiClasskun KyakuhonGoodCls;
+  VotingPage({Key key,@required this.user_id,@required this.user_email,@required this.GoodCls1,@required this.GoodCls2,@required this.GoodCls3,@required this.KyakuhonGoodCls,@required this.MVPCls}):super(key:key);
+  _VotingPageState createState() => _VotingPageState();
+}
+class _VotingPageState extends State<VotingPage>{
+  @override
+  Widget build(BuildContext context){
+    List<String> bunkasailskun=[];
+    bunkasailskun.add(BunkasaiClasskun_to_str(widget.GoodCls1));
+    bunkasailskun.add(BunkasaiClasskun_to_str(widget.GoodCls2));
+    bunkasailskun.add(BunkasaiClasskun_to_str(widget.GoodCls3));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("投票中..."),
+      ),
+      body:FutureBuilder(
+        future: sendKun(context),
+        builder: (BuildContext conkun,AsyncSnapshot<void> snapshot){
+          return ListView(
+            children: [
+              ListTile(
+                title: Text(widget.user_email)
+              ),
+              ListTile(
+                  title: Text(widget.user_id)
+              ),
+              ListTile(
+                  title: Text(BunkasaiClasskun_to_str(widget.MVPCls))
+              ),
+              ListTile(
+                  title: Text(BunkasaiClasskun_to_str(widget.KyakuhonGoodCls))
+              ),
+              ListTile(
+                  title: Text(bunkasailskun[0])
+              ),
+              ListTile(
+                  title: Text(bunkasailskun[1])
+              ),
+              ListTile(
+                  title: Text(bunkasailskun[2])
+              ),
+
+
+            ],
+          );
+        },
+      )
+    );
+  }
+  Future<void> sendKun(BuildContext context) async{
+
+  }
+}
