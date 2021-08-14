155d2390a49bd0eec4b96290a57c3bdbe53e6475
kokkiemouse <kokkiemouse@fascode.net>
2021-08-15 08:38:39 +0900

    added page

---
+++ lib/activities/admin/AdminListRank.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class AdminListRankPage extends StatefulWidget{
+  _AdminListRankState createState()=>_AdminListRankState();
+}
+class _AdminListRankState extends State<AdminListRankPage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Rank List"),),
+      body:FutureBuilder(
+        
+      )
+    );
+  }
+}
