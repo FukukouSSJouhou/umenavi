1b29d428ca86d794c14eb617f424a2c06e4fd772
kokkiemouse <kokkiemouse@fascode.net>
2021-08-31 07:30:02 +0900

    dev msg
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 setlocal
 set PATH=%LOCALAPPDATA%\JetBrains\Toolbox\apps\AndroidStudio\ch-0\202.7351085\jre\bin;%PATH%
 for /f "usebackq delims=" %%A in (`git rev-parse --short HEAD`) do set COMMITID=%%A
-set BUILDID=" %COMMITID% %date% %time%"
+set BUILDID=" %COMMITID% %date% %time% for Developer."
 rem echo %BUILDID%
 flutter build apk --dart-define=APPVER=%BUILDID%
